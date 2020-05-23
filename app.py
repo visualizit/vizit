aa887471fb717210c11b2ee7443020fd69e3fb8f
vizit <youngok2004@gmail.com>
2020-05-23 18:34:35 +0800

    add shapes for raft nodes

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import logging
-import queue
 from datetime import datetime
 from flask import Flask, request, jsonify
 from flask_socketio import SocketIO
@@ -11,7 +10,6 @@ logging.basicConfig(format='%(asctime)s - %(levelname)s: %(message)s', datefmt='
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socket_io = SocketIO(app)
-q = queue.Queue()
 
 
 @app.route('/monitor/action', methods=['POST'])
@@ -20,22 +18,10 @@ def monitor_action():
     return jsonify(action)
 
 
-@app.route('/view/status', methods=['POST'])
-def view_status():
-    leader = request.get_json()
-    logging.info(f' got heartbeat from leader: {leader}')
-    return {}
-
-
 @app.route('/', methods=['GET'])
 def index():
-    q.put(f'ct: {datetime.now()}')
-    socket_io.emit('update monitor', q.get(), namespace=NAMESPACE, callback=ack)
+    socket_io.emit('update monitor', f'now: {datetime.now()}', namespace=NAMESPACE)
     return 'hello'
-
-
-def ack():
-    print('message was received!')
 
 
 @socket_io.on('client action', namespace=NAMESPACE)
@@ -44,4 +30,4 @@ def handle_client_action(json):
 
 
 if __name__ == '__main__':
-    socket_io.run(app, port=8000)
+    socket_io.run(app)
